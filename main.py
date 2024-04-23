ffd918c87197670743b8ef42f629996600afbac0
Hugo Filipe Matos Almeida Moreira <122613212+benderpt@users.noreply.github.com>
2024-04-23 15:35:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ if st.session_state.answer_submitted:
                 pass
 
             st.markdown(""" ___""")
-            st.subheader("Se quiser acompanhar o progresso do Programa Nacional de Reformas, clique [aqui](https://www.portugal.gov.pt/pt/gc21/comunicacao/documento?i=programa-nacional-de-reformas-2021-2023), ou subscreva a nossa newsletter [aqui](https://planapp.gov.pt/)")
+            st.subheader("Acompanhe o trabalho do PlanAPP em [diferentes plataformas](https://linktr.ee/planapp).")
 
 else:
     if st.session_state.current_index < len(dados_quiz):
